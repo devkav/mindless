b72c4456e8b8ea2eea46ae094d649a0c5075c786
Devan Kavalchek <devankav7@gmail.com>
2026-04-17 23:46:00 -0400

    Added commit history (#5)

--- src/objects/commit.rs
+++ src/objects/commit.rs
@@ -1,6 +1,8 @@
+use std::fs::{read_to_string, write};
+
 use crate::{
     objects::{object::create_object, tree::create_tree},
-    util::{constants::COMMIT_OBJECT_TYPE, workspace::{get_nevermind_patterns, get_root, get_tracked_files}}
+    util::{constants::{COMMIT_OBJECT_TYPE, HEAD_FILE_NAME, MINDLESS_DIR_NAME, REFS_DIR_NAME}, mindless::get_head, workspace::{get_nevermind_patterns, get_root, get_tracked_files}}
 };
 
 
@@ -14,15 +16,13 @@ pub fn create_commit(message: &str) {
     let tracked_files = get_tracked_files(None, &nevermind_patterns, &mindless_root);
     let tree = create_tree(&mindless_root, &mindless_root, &tracked_files);
 
-    // TODO: Add parent!
+    let mut content = String::from(format!("tree {}\n", tree));
 
-    let content = format!(
-        "tree {}\n\
-        parent<PARENT>\n\
-        \n\
-        {}",
-        tree, message
-    );
+    if let Some(head_hash) = get_head(&mindless_root) {
+        content.push_str(&format!("parent {}\n", head_hash));
+    }
+
+    content.push_str(&format!("\n{}", message));
 
     let content_bytes = content.as_bytes();
 
@@ -31,5 +31,27 @@ pub fn create_commit(message: &str) {
     complete_bytes.extend(content_bytes);
 
     let commit_hash = create_object(&complete_bytes, &mindless_root);
-    println!("{}", commit_hash);
+
+    let head_file = mindless_root
+        .join(MINDLESS_DIR_NAME)
+        .join(HEAD_FILE_NAME);
+    let head_content = read_to_string(head_file).expect("Something went wrong while reading HEAD");
+    let tokens: Vec<&str> = head_content.split(" ").collect();
+
+    if tokens.len() != 2 {
+        println!("Something went wrong while committing to head");
+        return;
+    }
+
+    let ref_type = tokens[0];
+    let name = tokens[1];
+    let ref_folder = format!("{}s", ref_type);
+
+    let ref_file = mindless_root
+        .join(MINDLESS_DIR_NAME)
+        .join(REFS_DIR_NAME)
+        .join(ref_folder)
+        .join(name);
+
+    write(ref_file, commit_hash).expect("Something went wrong while committing hash");
 }
